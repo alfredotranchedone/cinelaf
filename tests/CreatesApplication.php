bb82bb9582995569b9620008d48a7d0d73c03a18
Alfredo Tranchedone <alfredo.tranchedone@yahoo.it>
2020-04-13 00:33:46 +0200

    add check for configuration cache

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -10,10 +10,16 @@ trait CreatesApplication
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
+     * @throws \Exception
      */
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
+
+        /* Controlla se la configurazione è in cache */
+        if( $app->configurationIsCached() ){
+            throw new \Exception('Configuration is cached! Any variable in phpunit.xml did not take effect. Run `$ php artisan config:clear`!');
+        }
 
         $app->make(Kernel::class)->bootstrap();
 
